d4dc238f182fedb907ac32f300c463f0e16cebea
h-dr4g0n <33449999+h-dr4g0n@users.noreply.github.com>
2017-12-16 14:33:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,9 @@ cd $HOME
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
 chmod 755 msfinstall && \
 ./msfinstall
+exit
+msfvenom
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
+cd ngrok
+chmod 770 *
