9fd987fb6d672833296b0f5e685acc46033af296
h-dr4g0n <33449999+h-dr4g0n@users.noreply.github.com>
2017-12-23 12:57:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ cd $HOME
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
 chmod 755 msfinstall && \
 ./msfinstall
+apt-get install git
+git config –global user.name h-dr4g0n
+git config –global user.email dragonmatrix2506@gmail.com
