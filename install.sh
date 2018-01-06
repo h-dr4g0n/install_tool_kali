4b9927130d73b2c71c2a93a8a155b1a90b99086e
h-dr4g0n <33449999+h-dr4g0n@users.noreply.github.com>
2018-01-07 06:19:21 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-cd $HOME
-curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
-chmod 755 msfinstall && \
-./msfinstall
-apt-get install git
+sudo apt-get install git
 git config –global user.name "h-dr4g0n"
 git config –global user.email "dragonmatrix2506@gmail.com"
-git clone https://github.com/trustedsec/social-engineer-toolkit/ set/
+export apktool_version=2.3.1
+sudo -E sh -c 'wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_$apktool_version.jar -O /usr/local/bin/apktool.jar'
+sudo chmod +r /usr/local/bin/apktool.jar
+sudo sh -c 'wget https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool -O /usr/local/bin/apktool'
+sudo chmod +x /usr/local/bin/apktool
